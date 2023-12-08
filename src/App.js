021a5afca1bb0570d8a65ee5ad55c50b5dae13f1
Mucahit <mucahit.kartal2015@gmail.com>
2023-12-08 13:34:47 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
 import data from "./data";
-import
+import SingleQuestion from "./Question";
+
+import { useState } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const [questions, setQuestions] = useState(data);
+
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and answers about login</h3>
+      </div>
+      <section className="info">
+        {questions.map((question) => {
+          return <SingleQuestion key={question.id} {...questions} />;
+        })}
+      </section>
+    </main>
+  );
 };
 
 export default App;
---
+++ src/Question.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const Question = () => {
+  return <div>Question</div>;
+};
+
+export default Question;
