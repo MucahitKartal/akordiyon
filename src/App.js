4217d4c102d404b00d42efab0cca06492fe16424
Mucahit <mucahit.kartal2015@gmail.com>
2023-12-08 15:20:51 +0300

    final

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import SingleQuestion from "./Question";
 import { useState } from "react";
 
 const App = () => {
-  const [questions, setQuestions] = useState(data);
+  // const [questions, setQuestions] = useState(data);
 
   return (
     <main>
       <div className="container">
         <h3>Questions and answers about login</h3>
+
+        <section className="info">
+          {data.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
       </div>
-      <section className="info">
-        {questions.map((question) => {
-          return <SingleQuestion key={question.id} {...questions} />;
-        })}
-      </section>
     </main>
   );
 };
